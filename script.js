d8504a282a7610948b61f3e81cedbd04f0fa04df
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-11-11 22:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+ async function delayAndShowText() {
+      const textInput = document.getElementById('text');
+      const delayInput = document.getElementById('delay');
+      const outputDiv = document.getElementById('output');
+
+      const text = textInput.value;
+      const delayInSeconds = parseFloat(delayInput.value);
+
+      if (!text || isNaN(delayInSeconds)) {
+        alert('Please enter valid text and delay.');
+        return;
+      }
+
+      outputDiv.innerHTML = ''; // Clear previous output
+
+      // Using async/await to introduce delay
+      await new Promise(resolve => setTimeout(resolve, delayInSeconds * 1000));
+
+      // Show the text after the delay
+      outputDiv.innerHTML = `<p>${text}</p>`;
+    }
+
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click', delayAndShowText);

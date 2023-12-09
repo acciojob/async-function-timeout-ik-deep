5b305496e1ec452079901fef5bf45acb5a9c2b42
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-12-09 10:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ async function showMessage() {
         const delayInMillis = delayInput * 1000;
 
         // Wait for the specified delay using setTimeout wrapped in a Promise
-        await new Promise(resolve => setTimeout(resolve, delayInMillis));
+        await setTimeout(()=>{
+			outputDiv.innerText = textInput;
+		} , delayInMillis));
 
         // Display the text after the delay
-        outputDiv.innerText = textInput;
+        
     } catch (error) {
         console.error('Error occurred:', error);
         outputDiv.innerText = 'An error occurred.';
@@ -23,3 +25,45 @@ async function showMessage() {
 
 // Attach event listener to the button
 document.getElementById('btn').addEventListener('click', showMessage);
+
+() => {
+  const delay = 1000;
+  const text = "Test - 1";
+
+  cy.visit(baseUrl + "/main.html");
+
+  // Ensure the output div is initially empty
+  cy.get("div#output").should("be.empty");
+
+  cy.get("input#text").type(text);
+  cy.get("input#delay").type(delay);
+  cy.get("button#btn").click();
+
+  // Wait for the delay period
+  cy.wait(delay);
+
+  // Check the content of div#output after the delay
+  cy.get("div#output").should("contain", text);
+}
+
+
+() => {
+  const delay = 2000;
+  const text = "Test - 2";
+
+  cy.visit(baseUrl + "/main.html");
+
+  // Ensure the output div is initially empty
+  cy.get("div#output").should("be.empty");
+
+  cy.get("input#text").type(text);
+  cy.get("input#delay").type(delay);
+  cy.get("button#btn").click();
+
+  // Wait for the delay period
+  cy.wait(delay);
+
+  // Check the content of div#output after the delay
+  cy.get("div#output").should("contain", text);
+}
+
